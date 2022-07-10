30c5175362ead15aa4acaeeb1cbbe137f6b72c9c
NZMM92 <50610570+NZMM92@users.noreply.github.com>
2022-07-10 14:30:10 +0800

    Add files via upload

--- Grocery_store_app.php
+++ Grocery_store_app.php
@@ -12,6 +12,7 @@ include "../Web_App/db_stuff/db.php";
     <title>Grocery shop</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=">
+    <meta name="keywords" content="HTML5,CSS,JavaScript, html5 session storage, html5 local storage">
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css" />
     <link href="css/master.css" rel="stylesheet" type="text/css" />
@@ -26,12 +27,11 @@ include "../Web_App/db_stuff/db.php";
 <body>
     <?php
     if (!isset($_SESSION['user_name'])) {
-        header("location: ../Web_App/login.php"); // redirects back to the login page 
-    } else {
-        $msg = "<h3> Welcome " . $_SESSION['user_name'] . "</h3>"; //this is shown on the navbar to show which is the user that is logged in
+        header("location: ../Web_App/index.php"); // redirects back to the login page 
     }
     ?>
     <?php if (isset($_SESSION['user_name'])) { ?>
+        <!-- navbar -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="logo-image">
@@ -43,9 +43,6 @@ include "../Web_App/db_stuff/db.php";
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" <?php echo $msg ?> </a>
-                    </li>
                     <li class="nav-item active">
                         <a class="nav-link">Home</a>
                     </li>
@@ -56,7 +53,7 @@ include "../Web_App/db_stuff/db.php";
                         <a class="nav-link" href="#product">Products</a>
                     </li>
                     <li class="nav-item">
-                        <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#cart"><i class="fa-solid fa-cart-shopping"></i>Cart</button>
+                        <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#cart" onclick="showCart();"><i class="fa-solid fa-cart-shopping"></i>Cart</button>
                     </li>
                     <li class="nav-item">
                         <button type="button" class="btn btn-dark" id="butt3" data-toggle="modal" data-target="#contactDetails">Contact Us</button>
@@ -80,148 +77,45 @@ include "../Web_App/db_stuff/db.php";
         <br />
         <br />
         <br />
-        <a id="category">
-            <h2 style="text-align: center">Categories</h2>
-            <div class="card-deck">
-                <div class="card card border-success" id="cat">
-                    <img class="card-img-top" src="images/cat_1.jpg">
-                    <div class="card-body">
-                        <h5 class="card-title">Vegetables</h5>
-                        <p class="card-text">upto 30% off</p>
-                        <br />
-                        <!-- "#product" -->
-                        <a class="btn btn-success btn-lg" href="https://impomu.com/" role="button"><span>Buy now </span></a>
-                    </div>
-                </div>
-                <div class="card card border-warning" id="cat">
-                    <img class="card-img-top" src="images/cat_2.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Fruits</h5>
-
-                        <p class="card-text">upto 44% off</p>
-                        <br>
-                        <!-- #product -->
-                        <a class="btn btn-success btn-lg" href="https://impomu.com/" role="button"><span>Buy now </span></a>
-                    </div>
-                </div>
-                <div class="card card border-danger" id="cat">
-                    <img class="card-img-top" src="images/cat_3.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Meat</h5>
-                        <p class="card-text">upto 50% off</p>
-                        <br>
-                        <!-- #product -->
-                        <a class="btn btn-success btn-lg" href="https://impomu.com/" role="button"><span>Buy now </span></a>
-                    </div>
+        <!-- category section -->
+        <div class="category"></div>
+        <h2 style="text-align: center">Categories</h2>
+        <div class="card-deck">
+            <div class="card card border-success" id="cat">
+                <img class="card-img-top" src="images/cat_1.jpg">
+                <div class="card-body">
+                    <h5 class="card-title">Vegetables</h5>
+                    <p class="card-text">upto 30% off</p>
+                    <br />
+                    <!-- "#product" -->
+                    <a class="btn btn-success btn-lg" href="https://impomu.com/" role="button"><span>Buy now </span></a>
                 </div>
             </div>
+            <div class="card card border-warning" id="cat">
+                <img class="card-img-top" src="images/cat_2.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Fruits</h5>
+                    <p class="card-text">upto 44% off</p>
+                    <br>
+                    <!-- #product -->
+                    <a class="btn btn-success btn-lg" href="https://impomu.com/" role="button"><span>Buy now </span></a>
+                </div>
+            </div>
+            <div class="card card border-danger" id="cat">
+                <img class="card-img-top" src="images/cat_3.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Meat</h5>
+                    <p class="card-text">upto 50% off</p>
+                    <br>
+                    <!-- #product -->
+                    <a class="btn btn-success btn-lg" href="https://impomu.com/" role="button"><span>Buy now </span></a>
+                </div>
+            </div>
+        </div>
         </a>
         <!-- Product Pages -->
-        <a id="product">
-            <h2 style="text-align: center">Products</h2>
-            <div class="produce" id="page_1">
-                <div class="card-deck">
-                    <div class="card border-success mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Orange</div>
-                        <div class="card-body">
-                            <img src="images/pro-1.jpg" width="200" height="150">
-                            <p class="card-text">Price: $2.50</p>
-                            <br>
-                            <a data-name="Orange" data-price="2.50" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                    <div class="card border-danger mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Strawberry</div>
-                        <div class="card-body">
-                            <img src="images/pro-2.jpg" width="200" height="150">
-                            <p class="card-text">Price: $2.70</p>
-                            <br>
-                            <a data-name="Strawberry" data-price="2.70" class="btn btn-success btn-lg add-to-cart " role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                    <div class="card border-warning mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Banana</div>
-                        <div class="card-body">
-                            <img src="images/pro-3.jpg" width="200" height="150">
-                            <p class="card-text">Price: $1.90</p>
-                            <br>
-                            <a data-name="Banana" data-price="1.90" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                    <div class="card border-primary mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Oat Milk</div>
-                        <div class="card-body">
-                            <img src="images/pro-4.jpg" width="200" height="150">
-                            <p class="card-text">Price: $2.00</p>
-                            <br>
-                            <a data-name="Oat Milk" data-price="2.00" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                    <div class="card border-danger mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Salmon</div>
-                        <div class="card-body">
-                            <img src="images/pro-5.jpg" width="200" height="150">
-                            <p class="card-text">Price: $10.50</p>
-                            <br>
-                            <a data-name="Salmon" data-price="10.50" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="produce" id="page_2" style="display: none">
-                <div class="card-deck">
-                    <div class="card border-danger mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Tomato</div>
-                        <div class="card-body">
-                            <img src="images/pro-6.jpg" width="200" height="150">
-                            <p class="card-text">Price: $3.50</p>
-                            <br>
-                            <a data-name="Tomato" data-price="3.50" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                    <div class="card border-warning mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Carrot</div>
-                        <div class="card-body">
-                            <img src="images/pro-7.jpg" width="200" height="150">
-                            <p class="card-text">Price: $1.80</p>
-                            <br>
-                            <a data-name="Carrot" data-price="1.80" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                    <div class="card border-warning mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Cheddar Cheese</div>
-                        <div class="card-body">
-                            <img src="images/pro-8.jpg" width="200" height="150">
-                            <p class="card-text">Price: $4.80</p>
-                            <br>
-                            <a data-name="Cheddar Cheese" data-price="4.80" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                    <div class="card border-danger mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Cherry Tomato</div>
-                        <div class="card-body">
-                            <img src="images/pro-9.jpg" width="200" height="150">
-                            <p class="card-text">Price: $5.60</p>
-                            <br>
-                            <a data-name="Cherry Tomato" data-price="5.60" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                    <div class="card border-danger mb-3" style="max-width: 18rem;">
-                        <div class="card-header">Apples</div>
-                        <div class="card-body">
-                            <img src="images/pro-10.jpg" width="200" height="150">
-                            <p class="card-text">Price: $1.60</p>
-                            <br>
-                            <a data-name="Apples" data-price="1.60" class="btn btn-success btn-lg add-to-cart" role="button"><span>Buy now</span></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="page_buttons">
-                <button type="button" class="btn btn-dark" id="butt1" onclick="producePage('page_1')">Page-1</button>
-                <button type="button" class="btn btn-dark" id="butt2" onclick="producePage('page_2')">Page-2</button>
-            </div>
-        </a>
+        <h2 style="text-align: center">Products</h2>
+        <?php include "../Web_App/db_stuff/get_product_from_db.php"; ?>
         <div class="modal fade" id="contactDetails" tabindex="-1" role="dialog" aria-labelledby="contactDetails" aria-hidden="true">
             <div class="modal-dialog modal-dialong-centered" role="document">
                 <div class="modal-content">
@@ -244,37 +138,41 @@ include "../Web_App/db_stuff/db.php";
         </div>
         <script src="js/xavierScripts.js" text="text/javascript"></script>
         <div class="modal fade" id="cart" tabindex="-1" role="dialog" aria-labelledby="cartTable" aria-hidden="true">
-            <div class="modal-dialog modal-dialong-centered" role="document">
+            <div class="modal-dialog modal-dialong-centered modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h4 class="modal-title" id="cartTItle">Cart</h4>
-                        <button type="button" class="btn btn-primary close" data-dismiss="modal" aria-label="close">
-                            <span aria-hidden="true"></span>
-                        </button>
+                        <button type="button" class="btn btn-primary close" data-dismiss="modal" aria-label="close"><span aria-hidden="true"></span></button>
                     </div>
                     <div class="modal-body">
-                        <section class="container content-section">
-                            <div class="cart-row">
-                                <span class="cart-item cart-header cart-column">Item</span>
-                                <span class="cart-price cart-header cart-column">Price</span>
-                                <span class="cart-quantity cart-header cart-column">Quantity</span>
-                            </div>
-                            <div class="cart-items">
-                            </div>
-                            <div class="cart-total">
-                                <strong class="cart-total-title">Total</strong>
-                                <span class="cart-total-price">$0</span>
-                            </div>
-                        </section>
+                        <table class="table" id="cartTable" style="width:100%">
+                            <thread>
+                                <tr>
+                                    <th style="border-bottom: 1px solid #000106;" scope="col">Product</th>
+                                    <th style="border-bottom: 1px solid #000106;" scope="col">Price</th>
+                                </tr>
+                            </thread>
+                            <tbody id="cartContents">
+                            </tbody>
+                        </table>
+                        <br>
+                        <h5 align="right">Total Price $<span id="modal_total-price"></span></h5>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-success" data-dismiss="modal">Close</button>
-                        <button class="btn btn-primary btn-purchase" type="button">Purchase</button>
+                    <div class="modal-footer border-top-0 d-flex justify-content-between">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-dark" onclick="ClearAll()">Clear Cart</button>
+                        <form action="../Web_App/cart_items.php" method="POST">
+                            <input type="hidden" id="naming" name = "product-name" value="">
+                            <input type="hidden" id="pricing" name = "product-price" value="">
+                            <button type="submit" class="btn btn-primary">Checkout</button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-
+        </div>
+        </div>
+        <script src="js/cart.js" type="text/javascript"></script>
     <?php } ?>
 </body>
 
--- index.php
+++ index.php
@@ -1,18 +1,29 @@
+<?php
+$msg = "";
+$button = "disabled";
+if (isset($_POST) & !empty($_POST)) {
+    if ($_POST['captcha']) {
+        $msg = "correct captcha";
+    } else {
+        $msg = "Invalid captcha";
+    }
+}
+if ($msg == "correct captcha") {
+    $button = "";
+} else {
+    $button = "disabled";
+}
+?>
 <!DOCTYPE html>
-<!--
-Allows user to login or register. You can choose to have a separate registration page.
-This page is deliberately left blank.
-
--->
 <html>
+
 <head>
     <meta charset="UTF-8">
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <script href="js/jquery-3.6.0.min.js" rel="stylesheet" type="text/javascript"></script>
-    <script href="js/bootstrap.min.js" rel="stylesheet" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
     <link href="css/login.css" rel="stylesheet" type="text/css" />
     <link href="css/master.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery-3.6.0.min.js" rel="stylesheet" type="text/javascript"></script>
+    <script src="js/bootstrap.bundle.min.js" rel="stylesheet" type="text/javascript"></script>
     <script src="js/openForm.js" type="text/javascript"></script>
     <title>Grocery shop</title>
     <link rel="icon" href="images/fu.ico" type="images/x-icon" />
@@ -30,27 +41,38 @@ This page is deliberately left blank.
         </nav>
         <br />
         <div class="formPages" id="registrationTab" style="display: none">
-            <legend class="registration-box" style="text-align: center; color: #000;">Register with US!</legend>
+            <legend class="registration-box" style="text-align: center; color: #000;">Registration Page</legend>
             <div class="control-group">
                 <form action="login_logout&registration/registration.php" method="post">
-                    <input class="form-control" type="text" placeholder="Username" name="username" oninvalid="alert('The username field must be filled');" required>
-                    <input class="form-control" type="password" placeholder="Password" name="password" oninvalid="alert('The password field must be filled');" required>
-                    <br />
-                    <div class="text-center">
-                        <button class="btn btn-outline-warning" type="submit">Sign Up</button>
+                    <input class="form-control" type="text" autocomplete="off" placeholder="Username" name="username" oninvalid="alert('The username field must be filled');" required>
+                    <input class="form-control" type="password" autocomplete="off" placeholder="Password" name="password" oninvalid="alert('The password field must be filled');" required>
+                    <input class="form-control" type="password" placeholder="Confirm Password" name="password" oninvalid="alert('The password field must be filled');" required>
+                    <input class="form-control" type="text" placeholder="Name" name="name" oninvalid="alert('The password field must be filled');" required>
+                    <input class="form-control" type="text" placeholder="Email Address" name="email" oninvalid="alert('The password field must be filled');" required>
+                    
+                    <br>
+                    <div style="text-align: center;">
+                        <img src="../Web_App/captcha/captcha.php" /><input class="form-control" type="text" placeholder="Enter Captcha here" name="captcha">
+                        <p style="color:#000 ;"><?php echo $msg ?></p>
+                        <input class="btn btn-warning" type="submit" name="submitCaptcha" value="Submit">
                     </div>
                 </form>
+                <br />
             </div>
         </div>
-        <div class="formPages" id="loginTab">
+        <div class="formPages" id="loginTab" >
             <legend class="login-box" style="text-align: center; color: #000;">Login Page</legend>
             <div class="loginPage">
                 <form class="form" action="login_logout&registration/loginProcess.php" method="post">
-                    <input class="form-control" type="text" placeholder="Username" name="username" oninvalid="alert('No username entered');" required>
-                    <input class="form-control" type="password" placeholder="Password" name="password" oninvalid="alert('No password entered');" required>
+                    <input class="form-control" type="text" autocomplete="off" placeholder="Username" name="username" oninvalid="alert('No username entered');" required>
+                    <input class="form-control" type="password" autocomplete="off" placeholder="Password" name="password" oninvalid="alert('No password entered');" required>
+                    <input class="form-control" type="number" placeholder="2FA code" name="MFA_code">
                     <br />
                     <div class="text-center">
-                        <button class="btn btn-outline-success" type="submit">Login</button>
+                        <button class="btn btn-success" type="submit">Login</button>
+                        <br>
+                        <br>
+                        <a href="../Web_App/forget_password/forgot_password.php">Forgot your password?</a>
                     </div>
                 </form>
             </div>
